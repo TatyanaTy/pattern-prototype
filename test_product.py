85d4d141a888d7a77ebe985bd39ed2dbdd54c5c2
TatyanaTy <tania20070202@gmail.com>
2026-06-19 00:26:53 +0300

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -60,10 +60,4 @@ def test_clone_all_attributes():
     assert cloned.color == original.color
 
 
-def test_this_will_fail():
-    """Тест 6: СПЕЦИАЛЬНО ПАДАЕТ - для демонстрации CI/CD"""
-    original = Product("Тест", "Тестовый", 100)
-    cloned = original.clone()
-    
-    # ЭТОТ ТЕСТ ВСЕГДА БУДЕТ ПАДАТЬ
-    assert original is cloned, "Демонстрационная ошибка для преподавателя"
+
